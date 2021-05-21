76fdcd72292c9eeec1327ae7c6ec322e81c2143d
Dani Coll <daniel.coll@dynatrace.com>
2021-05-21 13:36:10 +0200

    Next button & clear button fully working

--- popup.js
+++ popup.js
@@ -1,7 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-  var dropdown = document.getElementById('activate');
+  const nextButton = document.getElementById('next');
+  const clearButton = document.getElementById('clear');
+  const showResultsButton = document.getElementById('show-results');
 
-  dropdown.addEventListener('click', () => {
+  chrome.storage.sync.get(['index', 'randomRange'], ({index, randomRange}) => {
+    if(randomRange && index === randomRange.length) {
+      showResultsButton.style = "display: initial"
+      nextButton.style = "display: none"
+    }
+  });
+  nextButton.addEventListener('click', () => {
     runFunction(nextSpeaker)
+    chrome.storage.sync.get(['index', 'randomRange'], ({index, randomRange}) => {
+      if(randomRange && index === randomRange.length) {
+        showResultsButton.style = "display: initial"
+        nextButton.style = "display: none"
+      }
+    });  });
+
+  clearButton.addEventListener('click', () => {
+    runFunction(clearData)
+    nextButton.style = "display: initial"
+    showResultsButton.style = "display: none"
+  });
+
+  showResultsButton.addEventListener('click', () => {
+    runFunction(showHighlights)
   });
 });
