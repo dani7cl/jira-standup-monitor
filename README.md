747a3bc57ba8aef355cfe437a13a8b0c40d8f995
Dani Coll <daniel.coll@dynatrace.com>
2021-05-21 16:10:16 +0200

    Clean code & style buttons
    Former-commit-id: 6183baf1b2578a14a4fad2d7c9cedcdf4791fedf

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Jira Standupizer
-Decides who's next on standups
+Ramondly decides who's next on standups
+
+## User Guide
+ - Go to https://dev-jira.dynatrace.org/secure/RapidBoard.jspa
+ - Check all the team members are on a quick filter label and none is selected
+ - Open the extension
+ - Click the next button and let the randomly selected user talk
+ - Once it finished click next and repeat the process until everyone talked and the "Show results" button is visible
+ - Click "Show results" to the the times spent for everyone
+ - Finally, you can click "Clear data" to reset everything so you can start again
+
+
--- popup.js
+++ popup.js
@@ -1,31 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const nextButton = document.getElementById('next');
-  const clearButton = document.getElementById('clear');
-  const showResultsButton = document.getElementById('show-results');
+  const nextButton = document.getElementById('next')
+  const clearButton = document.getElementById('clear')
+  const showResultsButton = document.getElementById('show-results')
 
-  chrome.storage.sync.get(['index', 'randomRange'], ({index, randomRange}) => {
-    if(randomRange && index === randomRange.length) {
-      showResultsButton.style = "display: initial"
-      nextButton.style = "display: none"
+  chrome.storage.sync.get(['index', 'randomRange'], ({ index, randomRange }) => {
+    if (randomRange && index === randomRange.length) {
+      showResultsButton.style = 'display: initial'
+      nextButton.style = 'display: none'
     }
-  });
+  })
   nextButton.addEventListener('click', () => {
     runFunction(nextSpeaker)
-    chrome.storage.sync.get(['index', 'randomRange'], ({index, randomRange}) => {
-      if(randomRange && index === randomRange.length) {
-        showResultsButton.style = "display: initial"
-        nextButton.style = "display: none"
+    chrome.storage.sync.get(['index', 'randomRange'], ({ index, randomRange }) => {
+      if (randomRange && index === randomRange.length) {
+        showResultsButton.style = 'display: initial'
+        nextButton.style = 'display: none'
       }
-    });  
-  });
+    })
+  })
 
   clearButton.addEventListener('click', () => {
     runFunction(clearData)
-    nextButton.style = "display: initial"
-    showResultsButton.style = "display: none"
-  });
+    nextButton.style = 'display: initial'
+    showResultsButton.style = 'display: none'
+  })
 
   showResultsButton.addEventListener('click', () => {
     runFunction(showHighlights)
-  });
-});
+  })
+})
