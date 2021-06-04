50095925b413e21a13da5996f5d156eaf74b4cf0
Dani Coll <daniel.coll@dynatrace.com>
2021-06-04 10:06:55 +0200

    Add start button & unselect any previous speaker
    Former-commit-id: 9ae03cbede9092307e86fb983acb3ba5dc639b2c

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const startButton = document.getElementById('start')
   const nextButton = document.getElementById('next')
   const clearButton = document.getElementById('clear')
   const showResultsButton = document.getElementById('show-results')
@@ -8,20 +9,40 @@ document.addEventListener('DOMContentLoaded', () => {
       showResultsButton.style = 'display: initial'
       nextButton.style = 'display: none'
     }
+
+    if (index > 0) {
+      nextButton.style = 'display: initial'
+      startButton.style = 'display: none'
+      clearButton.style = 'display: initial'
+    }
   })
+
+  startButton.addEventListener('click', () => {
+    runFunction(unselectPreviousSpeakers)
+    runFunction(selectNextSpeaker)
+    startButton.style = 'display: none'
+    nextButton.style = 'display: initial'
+    clearButton.style = 'display: initial'
+  })
+
   nextButton.addEventListener('click', () => {
-    runFunction(nextSpeaker)
+    runFunction(unselectPreviousSpeakers)
+    runFunction(selectNextSpeaker)
     chrome.storage.sync.get(['index', 'randomRange'], ({ index, randomRange }) => {
       if (randomRange && index === randomRange.length) {
         showResultsButton.style = 'display: initial'
         nextButton.style = 'display: none'
+      }
+      if (!randomRange || index === 0) {
+        startButton.style = 'display: none'
       }
     })
   })
 
   clearButton.addEventListener('click', () => {
     runFunction(clearData)
-    nextButton.style = 'display: initial'
+    startButton.style = 'display: initial'
+    nextButton.style = 'display: none'
     showResultsButton.style = 'display: none'
   })
 
