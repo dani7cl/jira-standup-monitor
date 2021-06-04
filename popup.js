88c7a1f6a31eb04c60dfe5e06be7a84d76107e91
Dani Coll <daniel.coll@dynatrace.com>
2021-06-04 11:30:40 +0200

    Show speaker name & refactor show buttons functions
    Former-commit-id: 62de5b8e694669ad3285dd5302eef6eef4855fce

--- popup.js
+++ popup.js
@@ -1,29 +1,64 @@
+let speaker, advanceButtonsContainer, startButton, nextButton, skipButton, clearButton, showResultsButton
+
+function setSpeaker() {
+  setTimeout(() => {
+    chrome.storage.sync.get(['index', 'randomRange'], ({ index, randomRange }) => {
+      if (randomRange[randomRange.length - 1].endTime) {
+        speaker.style = 'visibility: hidden'
+      } else {
+        speaker.style = 'visibility: visible'
+        speaker.innerHTML = `${randomRange[index - 1].name}'s turn (Speaker n&ordm${index})`
+      }
+    })
+  }, 100)
+}
+
+function showAdvanceState() {
+  startButton.style = 'display: none'
+  advanceButtonsContainer.style = 'display: initial'
+  clearButton.style = 'display: initial'
+}
+
+function showResultsState() {
+  showResultsButton.style = 'display: initial'
+  startButton.style = 'display: none'
+  clearButton.style = 'display: initial'
+  advanceButtonsContainer.style = 'display: none'
+}
+
+function showEmptyInitialState() {
+  startButton.style = 'display: initial'
+  advanceButtonsContainer.style = 'display: none'
+  showResultsButton.style = 'display: none'
+  clearButton.style = 'display: none'
+  speaker.style = 'visibility: hidden'
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  const advanceButtonsContainer = document.getElementById('advance-buttons-container')
-  const startButton = document.getElementById('start')
-  const nextButton = document.getElementById('next')
-  const skipButton = document.getElementById('skip')
-  const clearButton = document.getElementById('clear')
-  const showResultsButton = document.getElementById('show-results')
+  speaker = document.getElementById('speaker')
+  advanceButtonsContainer = document.getElementById('advance-buttons-container')
+  startButton = document.getElementById('start')
+  nextButton = document.getElementById('next')
+  skipButton = document.getElementById('skip')
+  clearButton = document.getElementById('clear')
+  showResultsButton = document.getElementById('show-results')
 
   chrome.storage.sync.get(['index', 'randomRange'], ({ index, randomRange }) => {
     if (randomRange && index === randomRange.length) {
-      showResultsButton.style = 'display: initial'
-      startButton.style = 'display: none'
-      clearButton.style = 'display: initial'
+      showResultsState()
     } else if (index > 0) {
-      advanceButtonsContainer.style = 'display: initial'
-      startButton.style = 'display: none'
-      clearButton.style = 'display: initial'
+      showAdvanceState()
     }
   })
+
+  setSpeaker()
 
   startButton.addEventListener('click', () => {
     runFunction(unselectPreviousSpeakers)
     runFunction(selectNextSpeaker)
-    startButton.style = 'display: none'
-    advanceButtonsContainer.style = 'display: initial'
-    clearButton.style = 'display: initial'
+
+    showAdvanceState()
+    setSpeaker()
   })
 
   nextButton.addEventListener('click', () => {
@@ -31,27 +66,25 @@ document.addEventListener('DOMContentLoaded', () => {
     runFunction(selectNextSpeaker)
     chrome.storage.sync.get(['index', 'randomRange'], ({ index, randomRange }) => {
       if (randomRange && index === randomRange.length) {
-        showResultsButton.style = 'display: initial'
-        advanceButtonsContainer.style = 'display: none'
+        showResultsState()
       }
       if (!randomRange || index === 0) {
-        startButton.style = 'display: none'
+        showAdvanceState()
       }
     })
+    setSpeaker()
   })
 
   skipButton.addEventListener('click', () => {
     runFunction(unselectPreviousSpeakers)
     runFunction(skipCurrentSpeaker)
+    setSpeaker()
   })
 
   clearButton.addEventListener('click', () => {
     runFunction(unselectPreviousSpeakers)
     runFunction(clearData)
-    startButton.style = 'display: initial'
-    advanceButtonsContainer.style = 'display: none'
-    showResultsButton.style = 'display: none'
-    clearButton.style = 'display: none'
+    showEmptyInitialState()
   })
 
   showResultsButton.addEventListener('click', () => {
