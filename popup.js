4fedab022dd0d58242a0794a384b12eafa46ba90
Dani Coll <daniel.coll@dynatrace.com>
2021-06-04 10:41:27 +0200

    Add Skip button
    Former-commit-id: 2bc6e88fe46786c9d10721ddbccad17b4bc9c30c

--- popup.js
+++ popup.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const advanceButtonsContainer = document.getElementById('advance-buttons-container')
   const startButton = document.getElementById('start')
   const nextButton = document.getElementById('next')
+  const skipButton = document.getElementById('skip')
   const clearButton = document.getElementById('clear')
   const showResultsButton = document.getElementById('show-results')
 
   chrome.storage.sync.get(['index', 'randomRange'], ({ index, randomRange }) => {
     if (randomRange && index === randomRange.length) {
       showResultsButton.style = 'display: initial'
-      nextButton.style = 'display: none'
-    }
-
-    if (index > 0) {
-      nextButton.style = 'display: initial'
+      startButton.style = 'display: none'
+      clearButton.style = 'display: initial'
+    } else if (index > 0) {
+      advanceButtonsContainer.style = 'display: initial'
       startButton.style = 'display: none'
       clearButton.style = 'display: initial'
     }
@@ -21,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
     runFunction(unselectPreviousSpeakers)
     runFunction(selectNextSpeaker)
     startButton.style = 'display: none'
-    nextButton.style = 'display: initial'
+    advanceButtonsContainer.style = 'display: initial'
     clearButton.style = 'display: initial'
   })
 
@@ -31,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
     chrome.storage.sync.get(['index', 'randomRange'], ({ index, randomRange }) => {
       if (randomRange && index === randomRange.length) {
         showResultsButton.style = 'display: initial'
-        nextButton.style = 'display: none'
+        advanceButtonsContainer.style = 'display: none'
       }
       if (!randomRange || index === 0) {
         startButton.style = 'display: none'
@@ -39,11 +40,18 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   })
 
+  skipButton.addEventListener('click', () => {
+    runFunction(unselectPreviousSpeakers)
+    runFunction(skipCurrentSpeaker)
+  })
+
   clearButton.addEventListener('click', () => {
+    runFunction(unselectPreviousSpeakers)
     runFunction(clearData)
     startButton.style = 'display: initial'
-    nextButton.style = 'display: none'
+    advanceButtonsContainer.style = 'display: none'
     showResultsButton.style = 'display: none'
+    clearButton.style = 'display: none'
   })
 
   showResultsButton.addEventListener('click', () => {
